c7f3c684116964850e69f9fdd02ad6b128b66f0b
AKSHAT031027 <meenaakshat03@gmail.com>
2025-09-03 21:51:38 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,18 +1,10 @@
-<?php
-$host = "dpg-d2rg27adbo4c73d9ro2g-a";   // e.g. dpg-xxxxx.render.com
-$port = 5432;                   // PostgreSQL default
-$dbname = "navkardatabase";
-$user = "navkardatabase_user";
-$password = "e7mDVwBkOpvert2W2UE9OPYv14rLFijU";
 
+
+<?php
 try {
-    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
-    $conn = new PDO($dsn, $user, $password, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    ]);
+    $conn = new PDO("pgsql:host=localhost;port=5432;dbname=volunteer_db", "postgres", "Akshatmeena@1");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
 ?>
-

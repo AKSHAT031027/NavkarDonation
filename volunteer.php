3d1c4de25a00d0a62dae66f7d7597e04ac11c8fd
AKSHAT031027 <meenaakshat03@gmail.com>
2025-09-11 16:10:31 +0530

    Update volunteer.php

--- volunteer.php
+++ volunteer.php
@@ -3,7 +3,7 @@
 $host = "dpg-d2rg27adbo4c73d9ro2g-a.oregon-postgres.render.com";
 $dbname = "navkardatabase";
 $user = "navkardatabase_user";
-$password = "YOUR_PASSWORD"; // replace with actual password
+$password = "e7mDVwBkOpvert2W2UE9OPYv14rLFijU"; // replace with actual password
 $port = "5432";
 
 try {
@@ -85,3 +85,4 @@ $volunteers = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <?php endif; ?>
 </body>
 </html>
+

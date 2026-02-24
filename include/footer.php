2959d6aace08dde02bb4955a1804952e2fe65b85
SuyashJain752 <suyashjain752@gmail.com>
2026-02-24 16:20:59 +0530

    Update footer.php

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,5 @@
 <div footer style="background-color: #333; color: #fff; padding: 20px; text-align: center; margin-left: -5px;">
-    <p>&copy; 2025 Navkar Donations All rights reserved.</p>
+    <p>&copy; 2026 Navkar Donations All rights reserved.</p>
 
     <p>Follow us on:
         <a href="#" style="color: #fff; text-decoration: none;" class="fa ms-1 fa-facebook"> Facebook</a> |
@@ -7,3 +7,4 @@
         <a href="#" style="color: #fff; text-decoration: none;" class="fa ms-1 fa-instagram"> Instagram</a>
     </p>
 </div>
+
